8ab3091b0c243511c74ed8adf984856e69c73af9
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2022-10-23 19:06:50 +0530

    leetcode problem of day

--- set_mismatch.py
+++ set_mismatch.py
@@ -1,7 +1,13 @@
-def findErrorNums(nums):
+# solution 1 - using maths
+def findErrorNums1(nums):
     n = len(nums)
     A = -sum(nums) + n * (n + 1) // 2
     B = -sum(i * i for i in nums) + n * (n + 1) * (2 * n + 1) // 6
     return [(B - A * A) // 2 // A, (B + A * A) // 2 // A]
 
-print(findErrorNums([2, 3, 2]))
+# solution 2
+def findErrorNums2(self, nums):
+    return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
+
+
+print(findErrorNums1([2, 3, 2]))
